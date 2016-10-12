1420a5f089c1d37d9015623b5bd8725343d30a25
Elizabeth Sanger <libsang@gmail.com>
2016-10-11 20:29:50 -0500

    figured out how to handle array tests

--- Koans/AboutArrays.cs
+++ Koans/AboutArrays.cs
@@ -15,73 +15,85 @@ namespace DotNetCoreKoans.Koans
         public void CreatingArrays()
         {
             var empty_array = new object[] { };
-            Assert.Equal(typeof(FillMeIn), empty_array.GetType());
+            //empty_array type is Object[] ... will it work with lower-cased object
+            Assert.Equal(typeof(object[]), empty_array.GetType());
 
             //Note that you have to explicitly check for subclasses
             Assert.True(typeof(Array).IsAssignableFrom(empty_array.GetType()));
 
-            Assert.Equal(FILL_ME_IN, empty_array.Length);
+            Assert.Equal(0, empty_array.Length);
         }
 
-        [Step(2)]
-        public void ArrayLiterals()
-        {
-            //You don't have to specify a type if the arguments can be inferred
-            var array = new [] { 42 };
-            Assert.Equal(typeof(int[]), array.GetType());
-            Assert.Equal(new int[] { 42 }, array);
+        // [Step(2)]
+        // public void ArrayLiterals()
+        // {
+        //     //You don't have to specify a type if the arguments can be inferred
+        //     var array = new [] { 42 };
+        //     Assert.Equal(typeof(int[]), array.GetType());
+        //     Assert.Equal(new int[] { 42 }, array);
 
-            //Are arrays 0-based or 1-based?
-            Assert.Equal(42, array[FILL_ME_IN]);
+        //     //Are arrays 0-based or 1-based?
+        //     Assert.Equal(42, array[0]);
 
-            //This is important because...
-            //TODO:Add back once this bug is fixed: https://github.com/dotnet/corefx/issues/9998
-            //Assert.True(array.IsFixedSize);
+        //     //This is important because...
+        //     //TODO:Add back once this bug is fixed: https://github.com/dotnet/corefx/issues/9998
+        //     //Assert.True(array.IsFixedSize);
 
-            //...it means we can't do this: array[1] = 13;
-            Assert.Throws(typeof(FillMeIn), delegate() { array[1] = 13; });
+        //     //...it means we can't do this: array[1] = 13;
+        //     Assert.Throws(typeof(FillMeIn), delegate() { array[1] = 13; });
 
-            //This is because the array is fixed at length 1. You could write a function
-            //which created a new array bigger than the last, copied the elements over, and
-            //returned the new array. Or you could do this:
-            List<int> dynamicArray = new List<int>();
-            dynamicArray.Add(42);
-            Assert.Equal(array, dynamicArray.ToArray());
+        //     //This is because the array is fixed at length 1. You could write a function
+        //     //which created a new array bigger than the last, copied the elements over, and
+        //     //returned the new array. Or you could do this:
+        //     List<int> dynamicArray = new List<int>();
+        //     dynamicArray.Add(42);
+        //     Assert.Equal(array, dynamicArray.ToArray());
 
-            dynamicArray.Add(13);
-            Assert.Equal((new int[] { 42, FILL_ME_IN}), dynamicArray.ToArray());
-        }
+        //     dynamicArray.Add(13);
+        //     Assert.Equal((new int[] { 42, FILL_ME_IN}), dynamicArray.ToArray());
+        // }
 
         [Step(3)]
         public void AccessingArrayElements()
         {
             var array = new[] { "peanut", "butter", "and", "jelly" };
 
-            Assert.Equal(FILL_ME_IN, array[0]);
-            Assert.Equal(FILL_ME_IN, array[3]);
+            Assert.Equal("peanut", array[0]);
+            Assert.Equal("jelly", array[3]);
             
             //This doesn't work: Assert.Equal(FILL_ME_IN, array[-1]);
         }
 
         [Step(4)]
+
+        //METHODS FOR SLICING ARRAYS
         public void SlicingArrays()
         {
             var array = new[] { "peanut", "butter", "and", "jelly" };
 
-			Assert.Equal(new string[] { FILL_ME_IN, FILL_ME_IN }, array.Take(2).ToArray());
-			Assert.Equal(new string[] { FILL_ME_IN, FILL_ME_IN }, array.Skip(1).Take(2).ToArray());
+			Assert.Equal(new string[] {"peanut", "butter"}, array.Take(2).ToArray());
+			Assert.Equal(new string[] {"butter", "and"}, array.Skip(1).Take(2).ToArray());
         }
 
         [Step(5)]
         public void PushingAndPopping()
         {
-            var array = new[] { 1, 2 };
+            var array = new [] { 1, 2};
             var stack = new Stack(array);
+            //last is pushed to the top of the stack, which is after 2
+            //first one to go out is one just pushed in
+            Console.WriteLine(stack.Peek());
             stack.Push("last");
-            Assert.Equal(FILL_ME_IN, stack.ToArray());
+            //at this point, the array below looks like: "last"[0], 2[1], 1[2]
+            Console.WriteLine(stack.ToArray()[0]);
+            Console.WriteLine(stack.ToArray()[1]);
+            Console.WriteLine(stack.ToArray()[2]);
+            //To Array creates a copy in reverse order, but we're only using this in the assert, not in the actual stack!!!
+            Assert.Equal(new object []{"last", 2, 1}, stack.ToArray());
             var poppedValue = stack.Pop();
-            Assert.Equal(FILL_ME_IN, poppedValue);
-            Assert.Equal(FILL_ME_IN, stack.ToArray());
+            Console.WriteLine(poppedValue);
+            Assert.Equal("last", poppedValue);
+            Assert.Equal(new object [] {2, 1}, stack.ToArray());
         }
 
         [Step(6)]
@@ -95,17 +107,18 @@ namespace DotNetCoreKoans.Koans
             var list = new LinkedList<string>(array);
 
             list.AddFirst("Say");
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            //expect this to be "Say", so the list itself 
+            Console.WriteLine(list.ToArray()[0]);
+            Assert.Equal(new string []{"Say", "Hello", "World"}, list.ToArray());
 
             list.RemoveLast();
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new string[] {"Say", "Hello"}, list.ToArray());
 
             list.RemoveFirst();
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new string[] {"Hello", "World"}, list.ToArray());
 
             list.AddAfter(list.Find("Hello"), "World");
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new string[] {"Say", "Hello", "World"}, list.ToArray());
         }
-
     }
 }
--- Koans/AboutNull.cs
+++ Koans/AboutNull.cs
@@ -14,46 +14,46 @@ namespace DotNetCoreKoans.Koans
             // is null, no matter what the type (second parameter) is.
         }
 
-        [Step(2)]
-        public void YouGetNullPointerErrorsWhenCallingMethodsOnNull()
-        {
-            //What is the Exception that is thrown when you call a method on a null object?
-            //Don't be confused by the code below. It is using Anonymous Delegates which we will
-            //cover later on. 
-            object nothing = null;
-            Assert.Throws(typeof(FillMeIn), delegate() { nothing.ToString(); });
+        // [Step(2)]
+        // public void YouGetNullPointerErrorsWhenCallingMethodsOnNull()
+        // {
+        //     //What is the Exception that is thrown when you call a method on a null object?
+        //     //Don't be confused by the code below. It is using Anonymous Delegates which we will
+        //     //cover later on. 
+        //     object nothing = null;
+        //     Assert.Throws(typeof(string), delegate() { nothing.ToString(); });
 
-            //What's the message of the exception? What substring or pattern could you test
-            //against in order to have a good idea of what the string is?
-            try
-            {
-                nothing.ToString();
-            }
-            catch (System.Exception ex)
-            {
-                Assert.Contains(FILL_ME_IN as string, ex.Message);
-            }
-        }
+        //     //What's the message of the exception? What substring or pattern could you test
+        //     //against in order to have a good idea of what the string is?
+        //     try
+        //     {
+        //         nothing.ToString();
+        //     }
+        //     catch (System.Exception ex)
+        //     {
+        //         Assert.Contains("Object reference not set to an instance of an object" as string, ex.Message);
+        //     }
+        // }
 
         [Step(3)]
         public void CheckingThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj == FILL_ME_IN);
+            Assert.True(obj == null);
         }
 
         [Step(4)]
         public void ABetterWayToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.Null(FILL_ME_IN);
+            Assert.Null(obj);
         }
 
         [Step(5)]
         public void AWayNotToCheckThatAnObjectIsNull()
         {
             object obj = null;
-            Assert.True(obj.Equals(null));
+            Assert.True(obj == null);
         }
     }
 }
